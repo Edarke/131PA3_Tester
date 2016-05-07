dedb03ade18122ee05347619097c95efd2a7ad22
Evan Darke <edarke@ucsd.edu>
2016-05-07 01:31:47 -0700

    added two more tests

---
+++ nestedError.glsl
@@ -0,0 +1,25 @@
+void main(){
+        mat2 v;
+        if(true){
+        } else {
+	        int i;
+	        for(i = 0; i < 10; ++i){
+		        while(true){
+			        switch(i){
+				        case 1: 
+				        {
+					        int x = (i == true) ? 
+						        x + i
+						        : v[p++];
+
+					        switch(s){
+						        case 2: return;
+						        default: return true;
+					        }
+				        }
+			        }
+		        }
+	        }
+        }
+
+}
