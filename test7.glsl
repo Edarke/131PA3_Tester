93d17b5d3b7fa41313f6207e486b2f8d107bdf38
Evan Darke <edarke@ucsd.edu>
2016-05-08 12:27:06 -0700

    After EC, these should still fail.

---
+++ test7.glsl
@@ -0,0 +1,56 @@
+void main() {
+	float x = 1.0;
+	int x = 5 + 1.0;	//incompatible operands, and redeclaration
+}
+
+int lol()
+{
+	return 4;
+
+
+	int i = 0;
+	for(i = 0; (6.0 + 5) + 6.0; i++)
+	{
+
+	}
+
+	for(i = 0; (6.0 + 5) + 6.0; i++)
+		i = 3;
+
+	while((6.0 + 5) + 6.0)
+	{
+
+	}
+
+	while((6.0 + 5) + 6.0)
+	   i = 3;
+
+	if( (6.0 + 5) + 6.0 )
+	   i = 3;
+
+
+	if( (6.0 + 5) + 6.0 )
+	{
+	   i = 3;
+	}
+
+
+	if( (6.0 + 5) + 6.0 )
+	{
+	   i = 3;
+	}
+	else
+	{
+	
+	}
+
+
+
+}
+
+int test();
+
+int test()
+{
+   return 2;
+}
